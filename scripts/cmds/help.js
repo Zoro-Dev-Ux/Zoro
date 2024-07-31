a6c76553ff0a6efd66c5bb211172979a0f7784ad
Isaiah-ilom <139964528+Isaiah-ilom@users.noreply.github.com>
2024-07-31 20:44:27 +0100

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "【 https://facebook.com/the.king.of.ghost.black】";
+const doNotDelete = "【 https://www.facebook.com/profile.php?id=61553871285563】";
 /** 
 * @author NTKhang
 * @author: do not delete it
