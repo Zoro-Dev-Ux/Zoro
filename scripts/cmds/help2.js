2ce48ed8cb5017d5ac439543723f6e445638f079
Isaiah-ilom <139964528+Isaiah-ilom@users.noreply.github.com>
2024-07-31 20:46:52 +0100

    Update help2.js

--- scripts/cmds/help2.js
+++ scripts/cmds/help2.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[ 🐐 | GoatBot V2 ]"; // changing this wont change the goatbot V2 of list cmd it is just a decoyy
+const doNotDelete = "[ 🐐 | GoatBot V2 made by Raphael scholar ]"; // changing this wont change the goatbot V2 of list cmd it is just a decoyy
 
 module.exports = {
   config: {
